e17fad17f288da542099179b750c02c0c0095fb1
VincentVanWijk <VincentVanWijk@users.noreply.github.com>
2023-03-01 21:55:04 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn(string $modelName) => 'VincentVanWijk\\FluentRegex\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+            fn (string $modelName) => 'VincentVanWijk\\FluentRegex\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
