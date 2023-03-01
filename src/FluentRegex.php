47151482b3f1fbe28a7bf5fb467282b0501f2b0d
Vincent <vincent@avant.nl>
2023-03-01 19:32:09 +0100

    wip

--- src/FluentRegex.php
+++ src/FluentRegex.php
@@ -4,4 +4,26 @@ namespace VincentVanWijk\FluentRegex;
 
 class FluentRegex
 {
+    private string $subject;
+    private string $delimiter;
+    private string $regex = '';
+
+
+    public function __construct(string $subject, $delimiter = '/')
+    {
+        $this->subject = $subject;
+        $this->delimiter = $delimiter;
+
+    }
+
+    public function exactly(string $exactly)
+    {
+        $this->regex .= $exactly;
+    }
+
+    public function match(){
+        $this->regex = $this->delimiter . $this->regex . $this->delimiter;
+
+        return preg_match($this->regex, $this->subject);
+    }
 }
