abef7715bb195bd7709925c65370412801c10da1
VincentVanWijk <VincentVanWijk@users.noreply.github.com>
2023-03-01 18:37:18 +0000

    Fix styling

--- src/FluentRegex.php
+++ src/FluentRegex.php
@@ -5,29 +5,30 @@ namespace VincentVanWijk\FluentRegex;
 class FluentRegex
 {
     private string $subject;
-    private string $delimiter;
-    private string $regex = '';
 
+    private string $delimiter;
+
+    private string $regex = '';
 
     public function __construct(string $subject, $delimiter = '/')
     {
         $this->subject = $subject;
         $this->delimiter = $delimiter;
-
     }
 
-    public function exactly(string $exactly):FluentRegex
+    public function exactly(string $exactly): FluentRegex
     {
         $this->regex .= $exactly;
 
         return $this;
     }
 
-    public function match():array
+    public function match(): array
     {
-        $this->regex = $this->delimiter . $this->regex . $this->delimiter;
+        $this->regex = $this->delimiter.$this->regex.$this->delimiter;
         $matches = [];
         preg_match($this->regex, $this->subject, $matches);
+
         return $matches;
     }
 }
