9c14c6349a0cca5e69c57d9eee44d3e17e666fea
Vincent van Wijk <vincent@avant.nl>
2023-03-08 10:11:06 +0100

    escape the input in the quantifier methods

--- src/Traits/Quantifiers.php
+++ src/Traits/Quantifiers.php
@@ -4,41 +4,44 @@ declare(strict_types=1);
 
 namespace VincentVanWijk\FluentRegex\Traits;
 
-use VincentVanWijk\FluentRegex\FluentRegex;
-
 trait Quantifiers
 {
     public function zeroOrOne(string $string = ''): static
     {
-        $this->addToRegex($string . '?');
+        $string = $this->escape($string);
+        $this->addToRegex($string.'?');
 
         return $this;
     }
 
     public function zeroOrMore(string $string = ''): static
     {
-        $this->addToRegex($string . '*');
+        $string = $this->escape($string);
+        $this->addToRegex($string.'*');
 
         return $this;
     }
 
     public function oneOrMore(string $string = ''): static
     {
-        $this->addToRegex($string . '+');
+        $string = $this->escape($string);
+        $this->addToRegex($string.'+');
 
         return $this;
     }
 
     public function nTimes(int $times): static
     {
-        $this->addToRegex('{' . $times . '}');
+        $string = $this->escape($string);
+        $this->addToRegex('{'.$times.'}');
 
         return $this;
     }
 
     public function nTimesOf(string $string, int $times): static
     {
-        $this->addToRegex($string . '{' . $times . '}');
+        $string = $this->escape($string);
+        $this->addToRegex($string.'{'.$times.'}');
 
         return $this;
     }
